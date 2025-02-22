74bf7a7a54acc986a53ab871332fdf6949834b3e
Pranjali Sachan <pranjali@Kushs-MacBook.local>
2025-02-21 20:40:08 -0500

    fixed imports

--- domain/connection.js
+++ domain/connection.js
@@ -1,5 +1,5 @@
 const { createClient } = require('@supabase/supabase-js');
-const config = require('../../config');
+const config = require('../config');
 
 function client() {
     return createClient(config.SUPABASE.URL, config.SUPABASE.KEY);
